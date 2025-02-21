d6fbb4a4205cb5a0bb1995b39b2117fc84bb665d
Austen Collins <austencollins@gmail.com>
2025-02-21 10:34:52 -0800

    fix: astro example

--- example-astro-static/service/astro.config.mjs
+++ example-astro-static/service/astro.config.mjs
@@ -1,10 +1,24 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
+/**
+ * Astro configuration for Serverless Container Framework.
+ *
+ * @module AstroConfig
+ */
 export default defineConfig({
   output: 'server',
-  adapter: 'node',
+  adapter: node({
+    // Use 'standalone' mode so that the server code is bundled
+    mode: 'standalone'
+  }),
   server: {
     port: 8080,
     host: true
+  },
+  vite: {
+    server: {
+      allowedHosts: ['service'] // Named after your container name
+    }
   }
 });
